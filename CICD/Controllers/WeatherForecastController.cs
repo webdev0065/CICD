2258ee6163bfff396ef1820aad8dda5576a09b70
webdev0065 <webdev0065@gmail.com>
2025-06-30 15:52:14 +0530

    commit

--- CICD/Controllers/WeatherForecastController.cs
+++ CICD/Controllers/WeatherForecastController.cs
@@ -20,7 +20,6 @@ namespace CICD.Controllers
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
-         
         {
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
