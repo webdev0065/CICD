e8aa6aa288bf5303e5997c7f126ff07ad04a47c4
webdev0065 <webdev0065@gmail.com>
2025-06-30 13:00:05 +0530

    commit

--- CICD/Controllers/WeatherForecastController.cs
+++ CICD/Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace CICD.Controllers
          
         {
 
-            Console.WriteLine("Hello in branch!");
+            Console.WriteLine("Hello in b ranch!");
 
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
