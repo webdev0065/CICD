83893688fc4d615a9b335096f1a5faf9684ed82e
webdev0065 <webdev0065@gmail.com>
2025-06-30 11:40:26 +0530

    commot

--- CICD/Controllers/WeatherForecastController.cs
+++ CICD/Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace CICD.Controllers
          
         {
 
-            Console.WriteLine("Hello World from dsfsfgsbg!");
+            Console.WriteLine("Hello World from dsfsfg!");
 
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
